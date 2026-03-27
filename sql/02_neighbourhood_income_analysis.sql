aa03e3c1a3af1a7b3aa72bd710649e553041018f
kudzainyakudya-cmyk <kudzainyakudya@gmail.com>
2026-03-26 20:47:53 -0700

    Update 02_neighbourhood_income_analysis.sql

--- sql/02_neighbourhood_income_analysis.sql
+++ sql/02_neighbourhood_income_analysis.sql
@@ -20,3 +20,26 @@ CROSS JOIN overdose_deaths o
 WHERE o.city = 'Vancouver'
 GROUP BY n.neighbourhood, n.median_income_2015
 ORDER BY n.median_income_2015 ASC;
+
+-- Query 3: Neighbourhood vulnerability ranking by income
+SELECT 
+    neighbourhood,
+    median_income_2015,
+    RANK() OVER (ORDER BY median_income_2015 ASC) AS vulnerability_rank,
+    CASE 
+        WHEN median_income_2015 < 27000 THEN 'High Risk'
+        WHEN median_income_2015 BETWEEN 27000 AND 35000 THEN 'Moderate Risk'
+        ELSE 'Lower Risk'
+    END AS risk_category
+FROM neighbourhood_income
+ORDER BY vulnerability_rank ASC;
+
+-- Query 4: Income gap between Vancouver's wealthiest and most vulnerable neighbourhoods
+SELECT 
+    MAX(median_income_2015) AS highest_income_neighbourhood,
+    MIN(median_income_2015) AS lowest_income_neighbourhood,
+    MAX(median_income_2015) - MIN(median_income_2015) AS income_gap,
+    ROUND(CAST(MAX(median_income_2015) AS FLOAT) / MIN(median_income_2015), 1) AS income_ratio,
+    (SELECT neighbourhood FROM neighbourhood_income WHERE median_income_2015 = MAX(n.median_income_2015)) AS richest,
+    (SELECT neighbourhood FROM neighbourhood_income WHERE median_income_2015 = MIN(n.median_income_2015)) AS poorest
+FROM neighbourhood_income n;
